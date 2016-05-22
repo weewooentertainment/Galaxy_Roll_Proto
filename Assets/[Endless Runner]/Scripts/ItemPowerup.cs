5ebef8d2a324421fe50aec3763ed2330b9ddd136
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-05-22 17:44:27 -0400

    Initial commit of Galaxy Roll Prototype

---
+++ Assets/[Endless Runner]/Scripts/ItemPowerup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace EndlessRunner
+{
+    [AddComponentMenu("CUSTOM / Item Powerup")]
+    public class ItemPowerup : ItemBase
+    {
+        public override int HitPoints { get { return 200; } }
+
+        public override void Reset()
+        {
+            transform.localPosition = new Vector3(Random.Range(-5f, 5f), 0, 0);
+        }
+
+        protected override void OnCollisionEnter(Collision collision)
+        {
+            if (collision.IsPlayer())
+            {
+				GlobalVariables.Player.ActivateDoubleJumpAbility();
+
+                Hide();
+
+                SoundManager.PlaySoundEffect("SpecialItemHit");
+
+                GameDirector.AddScore(HitPoints);
+            }
+        }
+    }
+}
